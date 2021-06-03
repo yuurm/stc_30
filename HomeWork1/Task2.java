863d916db12305b52fe74825e09b34c112a07649
Yuri Urmatsky <yuriurmatsky@gmail.com>
2021-06-03 15:53:13 +0300

    binary representation/Task2.java/binary representation of a five-digit number

---
+++ HomeWork1/Task2.java
@@ -0,0 +1,56 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int number = 12345; // заданное число
+        int binaryNumber = 0; // начальное значение бинарного представления числа
+
+        int number1 = number % 2; // 14-я цифра бинарного представления числа - остаток от деления
+        int intQuotient1 = number / 2; // целочисленное частное 1-го деления
+        int number2 = intQuotient1 % 2; // 13-я цифра бинарного представления числа - остаток от деления
+        int intQuotient2 = intQuotient1 / 2; // целочисленное частное 2-го деления
+        int number3 = intQuotient2 % 2; // 12-я цифра бинарного представления числа - остаток от деления
+        int intQuotient3 = intQuotient2 / 2; // целочисленное частное 3-го деления
+        int number4 = intQuotient3 % 2; // 11-я цифра бинарного представления числа - остаток от деления
+        int intQuotient4 = intQuotient3 / 2; // целочисленное частное 4-го деления
+        int number5 = intQuotient4 % 2; // 10-я цифра бинарного представления числа - остаток от деления
+        int intQuotient5 = intQuotient4 / 2; // целочисленное частное 5-го деления
+        int number6 = intQuotient5 % 2; // 9-я цифра бинарного представления числа - остаток от деления
+        int intQuotient6 = intQuotient5 / 2; // целочисленное частное 6-го деления
+        int number7 = intQuotient6 % 2; // 8-я цифра бинарного представления числа - остаток от деления
+        int intQuotient7 = intQuotient6/ 2; // целочисленное частное 7-го деления
+        int number8 = intQuotient7 % 2; // 7-я цифра бинарного представления числа - остаток от деления
+        int intQuotient8 = intQuotient7 / 2; // целочисленное частное 8-го деления
+        int number9 = intQuotient8 % 2; // 6-я цифра бинарного представления числа - остаток от деления
+        int intQuotient9 = intQuotient8 / 2; // целочисленное частное 1-го деления
+        int number10 = intQuotient9 % 2; // 5-я цифра бинарного представления числа - остаток от деления
+        int intQuotient10 = intQuotient9 / 2; // целочисленное частное 1-го деления
+        int number11 = intQuotient10 % 2; // 4-я цифра бинарного представления числа - остаток от деления
+        int intQuotient11 = intQuotient10 / 2; // целочисленное частное 1-го деления
+        int number12 = intQuotient11 % 2; // 3-я цифра бинарного представления числа - остаток от деления
+        int intQuotient12 = intQuotient11 / 2; // целочисленное частное 1-го деления
+        int number13 = intQuotient12 % 2; // 2-я цифра бинарного представления числа - остаток от деления
+        int intQuotient13 = intQuotient12 / 2; // целочисленное частное 1-го деления
+        int number14 = intQuotient13 % 2; // 1-я цифра бинарного представления числа - остаток от деления
+
+
+
+
+        System.out.print(number14);
+        System.out.print(number13);
+        System.out.print(number12);
+        System.out.print(number11);
+        System.out.print(number10);
+        System.out.print(number9);
+        System.out.print(number8);
+        System.out.print(number7);
+        System.out.print(number6);
+        System.out.print(number5);
+        System.out.print(number4);
+        System.out.print(number3);
+        System.out.print(number2);
+        System.out.println(number1);
+
+
+
+
+    }
+}
